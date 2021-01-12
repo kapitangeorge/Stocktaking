1402f45ecb2d6de5ee593dc028da62bf82ad0ebc
goshankap <goshankap@gmail.com>
2021-01-12 18:17:23 +0500

    Список сотрудников

---
+++ Stocktaking/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stocktaking.Data;
+using Stocktaking.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stocktaking.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationContext database;
+
+        public UserController(ApplicationContext context)
+        {
+            database = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllUsers()
+        {
+            User user = await database.Users.FirstOrDefaultAsync(r => r.Username == User.Identity.Name);
+            var users = database.Users.Where(r => r.OrganizationId == user.OrganizationId).ToList();
+            return View(users);
+        }
+    }
+}
