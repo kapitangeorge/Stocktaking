88e5cd4b00e05c47785021a43ddd8f707c99c949
goshankap <goshankap@gmail.com>
2021-01-12 18:49:38 +0500

    Список всех предметов предприятия

---
+++ Stocktaking/Controllers/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stocktaking.Data;
+using Stocktaking.Data.Models;
+using Stocktaking.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stocktaking.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private ApplicationContext database;
+
+        public ItemController(ApplicationContext context)
+        {
+            database = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllItems()
+        {
+            User user = await database.Users.FirstOrDefaultAsync(r => r.Username == User.Identity.Name);
+            if(user != null)
+            {
+                var items = database.Items.Where(r => r.OrganizationId == user.OrganizationId);
+                if (items != null)
+                {,
+                    var itemsViewModels = new List<ItemInRoomViewModel>();
+                    foreach (var item in items)
+                    {
+                        var room = await database.Rooms.FirstOrDefaultAsync(r => r.Id == item.RoomId);
+                        itemsViewModels.Add(new ItemInRoomViewModel { Name = item.Name, Description = item.Description, Location = item.Location, InventoryNumber = item.InventoryNumber, Status = item.Status, RoomName = room.Name });
+                    }
+                    return View(itemsViewModels);
+                }
+
+                
+            }
+            return RedirectToAction("Login", "Account");
+        } 
+
+    }
+}
---
+++ Stocktaking/Data/ViewModels/ItemInRoomViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stocktaking.Data.ViewModels
+{
+    public class ItemInRoomViewModel
+    {
+        public string InventoryNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string RoomName { get; set; }
+
+        public string Status { get; set; }
+
+        public string Location { get; set; }
+    }
+}
